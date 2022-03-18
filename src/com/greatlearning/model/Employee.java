00e95dcada9fc97f1923e0dc40ebc485ba7f90d6
Piyush <piyush.bhele1995@hotmail.com>
2022-03-18 21:00:00 +0530

    changes made.

--- src/com/greatlearning/model/Employee.java
+++ src/com/greatlearning/model/Employee.java
@@ -4,7 +4,7 @@ public class Employee {
     private String firstName;
     private String lastName;
 
-    public Employee(String firstName, String lastName){
+    public Employee(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
--- src/com/greatlearning/service/CredentialService.java
+++ src/com/greatlearning/service/CredentialService.java
@@ -6,35 +6,34 @@ import java.util.Random;
 
 public class CredentialService {
 
-    public String generateEmailAddress(String firstName, String lastName, String department){
-        return firstName+lastName+"@"+department+".abc.com";
+
+    String chars = "abcdefghijklmnopqrstuvwxyz";
+    String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String NUM = "1234567890";
+    String SPEC = "@#$%^&+=";
+    Random rnd = new Random();
+
+
+    public String getTwoValue(String src) {
+
+        int index1 = (int) (rnd.nextFloat() * src.length()),
+                index2 = (int) (rnd.nextFloat() * src.length());
+        return "" + src.charAt(index1) + src.charAt(index2);
+    }
+
+    public String generatePassword() {
+        return getTwoValue(chars) + getTwoValue(CHARS) + getTwoValue(NUM) + getTwoValue(SPEC);
     }
 
 
-    public char [] generatePassword(){
-
-        String capitalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String smallLetters = "abcdefghijklmnopqrstuvwxyz";
-        String numbers = "1234567890";
-        String specialChar = "!@#$%^&*()_+-=/?><>>.";
-
-        String ranPass = capitalLetter + smallLetters + numbers + specialChar;
-
-        Random random = new Random();
-
-        char [] password = new char[8];
-
-        for ( int i = 0; i < 8; i++){
-
-            password[i] = ranPass.charAt(random.nextInt(ranPass.length()));
-        }
-        return password;
+    public void showCredentials(Employee employee, String email, String generatedPassword) {
+        System.out.println("Dear " + employee.getFirstName() + " your credentials are :");
+        System.out.println("Email : " + email);
+        System.out.println("Password : " + generatedPassword);
     }
 
-    public void showCredentials(Employee employee, String email, char [] generatedPassword){
-        System.out.println("Dear "+employee.getFirstName()+" your credentials are :");
-        System.out.println("Email : "+email);
-        System.out.println("Password : "+generatedPassword);
+    public String generateEmailAddress(String firstName, String lastName, String department) {
+        return firstName + lastName + "@" + department + ".abc.com";
     }
 
 
